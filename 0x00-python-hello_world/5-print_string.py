9c901d4e398b61bb754cde634db4b9a6b2062dcb
victoromuya <viczik16@gmail.com>
2024-01-02 12:46:45 -0800

    next com

---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+strtxt = "Holberton School"
+print(strtxt * 3)
+print(strtxt[:9])
---
+++ 0x00-python-hello_world/6-concat.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+txt1 = "Holberton"
+txt2 = "School"
+txt3 = txt1 + ' ' + txt2
+print(f"Welcome to {txt3}!")
