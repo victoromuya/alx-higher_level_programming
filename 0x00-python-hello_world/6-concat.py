a74f9984ad68b7e8660afb486c9a7d0fc20bbcfa
victoromuya <viczik16@gmail.com>
2024-01-02 13:01:39 -0800

    nc

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-txt1 = "Holberton"
-txt2 = "School"
-txt3 = txt1 + ' ' + txt2
-print(f"Welcome to {txt3}!")
+str1 = "Holberton"
+str2 = "School"
+str1 = str1 + ' ' + str2
+print(f"Welcome to {str1}!")
